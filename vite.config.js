bfc3710a920cd7fbc8f368caf67196b1c8a786c0
Jordan Rinder <jrinder42@users.noreply.github.com>
2026-03-28 20:11:20 -0400

    update configs to add testing libraries

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), svelteTesting()],
+
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.ts"],
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
